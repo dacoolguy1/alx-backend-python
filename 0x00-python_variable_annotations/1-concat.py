e4e4b4bd0ac2b3a422849744a24dc5b7ec1370b2
Emmanuel Temiede <68757523+dacoolguy1@users.noreply.github.com>
2023-07-07 01:57:37 +0100

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 def concat(str1: str, str2: str) -> str:
-    return str1 + str2
+    """Concatenate two strings
+
+    Args:
+        str1 (str): First string
+        str2 (str): Second string
+
+    Return:
+        (str) - concatenated string
+    """
+    return ''.join([str1, str2])
+    # return str1 + str2
