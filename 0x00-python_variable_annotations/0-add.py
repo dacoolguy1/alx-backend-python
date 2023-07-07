81c009ed41a923c8b47305b611719f317aa27350
dacoolguy1 <emmanueltemiede@gmail.com>
2023-07-06 17:43:42 -0700

    2

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
+"""Type-annotated function add"""
 def add(a: float, b: float) -> float:
     # print(a + b)
+    """Add two floats
+
+    Args:
+        a (float): first integer
+        b (float): second integer
+
+    Return:
+        sum(float)
+    """
     return a + b
